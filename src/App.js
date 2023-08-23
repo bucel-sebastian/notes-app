a6c084f541efb4a71df2d7526b534eaf6ac4a48d
Bucel Sebastian <bucel.ionsebastian@gmail.com>
2023-08-23 07:06:25 +0300

    saving notes in localStorage

--- src/App.js
+++ src/App.js
@@ -4,81 +4,66 @@ import OpenNote from './components/OpenNote'
 import { nanoid } from 'nanoid';
 
 import {FaPlus} from 'react-icons/fa';
+import AddNote from './components/AddNote';
 
 function App() {
 
-  const [notes, setNotes] = useState([
-        {
-          id: nanoid(),
-          title: "First note title",
-          star: false,
-          date: "15/04/2023",
-          content: "Text of this first note"
-        },
-        {
-          id: nanoid(),
-          title: "Second note title",
-          star: true,
-          date: "16/04/2023",
-          content: "Text of this second note"
-        },
-        {
-          id: nanoid(),
-          title: "Third note title",
-          star: false,
-          date: "15/04/2023",
-          content: "Text of this third note"
-        },
-        {
-          id: nanoid(),
-          title: "Fourth note title",
-          star: false,
-          date: "15/04/2023",
-          content: "Text of this fourth note"
-        },
-        {
-          id: nanoid(),
-          title: "Fifth note title",
-          star: false,
-          date: "15/04/2023",
-          content: "Text of this fifth note"
-        }
-    ]);
+  const initNotes = () => {
+    localStorage.setItem('notesList',JSON.stringify([]));
+    return '[]';
+  }
 
+  const [notes, setNotes] = useState(localStorage.getItem('notesList') ? (JSON.parse(localStorage.getItem('notesList'))) : (JSON.parse(initNotes())));
 
-  console.log(notes.length);
-
-    useEffect(()=>{
-      const storedNotes = localStorage.getItem('notesList');
-
-      if (storedNotes === null) {
-        localStorage.setItem('notesList',JSON.stringify(notes))
-      } else {
-        setNotes(JSON.parse(localStorage.getItem('notesList')))
-      }
-      
-    },[]);
 
   const [openedNote, setOpenedNote] = useState(null);
 
+  useEffect(()=>{
+    localStorage.setItem('notesList',JSON.stringify(notes));
+  },[notes]);
+
+
+  
+  console.log(notes);
+
+  const addNote = (title,content) => {
+
+
+    const date = new Date();
+
+    const newNote = {
+      id: nanoid(),
+      title: title,
+      star: false,
+      date: date.toLocaleDateString("en-GB"),
+      content: content
+    }
+
+    const newNotes = [...notes,newNote];
+    setNotes(newNotes);
+    switchFromNewToEdit(newNote.id);
+  }
+
+  const handleNewNoteBtn = () => {}
+
+
+  const switchFromNewToEdit = (id) => {
     
-
-  const handleNewNoteBtn = () => {
-
   }
 
   return (
     <div className='app-container'>
       {/* <OpenNote /> */}
+      <AddNote handleAddNote={addNote} />
       <div className='app-header'>
         <h2>Notes list</h2>
         <button className='add-note-btn' onClick={handleNewNoteBtn}><FaPlus/>New note</button>
       </div>
-      {notes.length === '0' ? 
-      (<h3>Nothing to see here</h3>) : 
-(<NotesList notes={notes}/>)
+      {
+        notes.length === '0' ? 
+        (<h3>Nothing to see here</h3>) : 
+        (<NotesList notes={notes}/>)
       }
-      
     </div>
   )
 }
---
+++ src/components/AddNote.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import {FaStar,FaRegStar,FaTimes} from "react-icons/fa";
+
+function AddNote({handleAddNote}) {
+
+    const [noteTitle,setNoteTitle] = useState('');
+    const [noteContent,setNoteContent] = useState('');
+
+    const handleCloseNewNote = (event) => {
+        event.stopPropagation();
+        console.log("Delete note ");
+    }
+
+    const handleCloseNote = ()=>{
+        console.log("Close note");
+    }
+
+    const handleSaveNewNote = ()=>{
+        if(noteTitle === '' && noteContent === ''){
+            return;
+        }
+        handleAddNote(noteTitle,noteContent)
+    }
+
+    const handleNoteTitleChange = (event) => {
+        setNoteTitle(event.target.value);
+    }
+
+    const handleNoteContentChange = (event) => {
+        setNoteContent(event.target.value);
+    }
+
+  return (
+    <div className='open-note-container' onClick={handleCloseNote}>
+        <div className='open-note-box' onClick={(event)=>{event.stopPropagation();}}>
+            <div className='note-header'>
+                <div className='note-header-first-row'>
+                    <div className='note-header-first-row-col'>
+                        
+                        <h3 className='note-title'>
+                            <input placeholder='Note title' className='edit-note-title' maxLength={50} value={noteTitle} onChange={handleNoteTitleChange}/>
+                        </h3>
+                    </div>
+                    <button onClick={handleSaveNewNote}>
+                        Save
+                    </button>
+                    <button className='note-trash-btn' onClick={handleCloseNewNote}>
+                        <FaTimes />
+                    </button>
+                </div>
+                <small className='note-date'>
+                    21/08/2023
+                </small>
+                
+
+            </div>
+            <div className='note-content'>
+                <textarea placeholder='Note content' className='edit-note-content' value={noteContent} onChange={handleNoteContentChange} />
+
+            
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default AddNote
